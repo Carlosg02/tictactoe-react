9597fbcc42be65d64f9f7ff7b65eae55dc4db952
Alberto Garro <carlosgaro11@gmail.com>
2022-07-14 15:46:15 -0400

    Updates

---
+++ precache-manifest.a16a5df7e05d1f511e73fbbf7476fe51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39602e7ab59414202b9",
+    "url": "/tictactoe-react/static/css/main.df8c45a1.chunk.css"
+  },
+  {
+    "revision": "a39602e7ab59414202b9",
+    "url": "/tictactoe-react/static/js/main.a39602e7.chunk.js"
+  },
+  {
+    "revision": "55971ed0781b9e766e30",
+    "url": "/tictactoe-react/static/js/1.55971ed0.chunk.js"
+  },
+  {
+    "revision": "fe3c286524cc5894d260",
+    "url": "/tictactoe-react/static/js/runtime~main.fe3c2865.js"
+  },
+  {
+    "revision": "90915ad3d89cede0c3b8b6fe3d205f43",
+    "url": "/tictactoe-react/index.html"
+  }
+];
